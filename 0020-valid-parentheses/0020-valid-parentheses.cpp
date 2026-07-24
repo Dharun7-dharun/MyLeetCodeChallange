86c4cf1330b5fbe62361aa9f27899c3dd0bc3166
Dharun s <kumardharun056@gmail.com>
2026-07-24 16:51:41 +0530

    Time: 0 ms (100%), Space: 8.9 MB (68.41%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -1,30 +1,28 @@
-#include <iostream>
+#include<stack>
 using namespace std;
 
-class Solution {
-public:
-    bool isValid(string s) {
+class Solution{
+    public:
+    bool isValid(string s){
         stack<char> st;
-
-        for (char ch : s) {
-            // Push opening brackets
-            if (ch == '(' || ch == '{' || ch == '[') {
-                st.push(ch);
-            }
-            // Check closing brackets
-            else {
-                if (st.empty())
-                    return false;
-
-                if ((ch == ')' && st.top() != '(') ||
-                    (ch == '}' && st.top() != '{') ||
-                    (ch == ']' && st.top() != '['))
-                    return false;
-
-                st.pop();
-            }
+        for(char ch :s){
+        if(ch =='(' || ch =='{' || ch =='[' ){
+            st.push(ch);
+            
         }
 
-        return st.empty();
+        else{
+            if(st.empty())
+                return false;
+        
+        if((ch ==')' && st.top() !='(' )|| 
+        (ch =='}' && st.top() !='{' )||
+        (ch ==']' && st.top() !='[' ))
+        return false;
+        st.pop();
     }
-};
+}
+return st.empty();
+}
+
+    };
