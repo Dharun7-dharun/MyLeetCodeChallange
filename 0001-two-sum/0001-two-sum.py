64ec07533d4578645df9330da2334d9220bc6ad7
Dharun s <kumardharun056@gmail.com>
2026-08-18 06:59:32 +0530

    Time: 2159 ms (18.55%), Space: 13.3 MB (16.74%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -4,23 +4,3 @@ class Solution(object):
             for j in range(i+1,len(nums)):
                 if nums[i]+nums[j]==target:
                     return [i,j]
-        
-
-
-#         class Solution {
-# public:
-#     vector<int> twoSum(vector<int>& nums, int target) {
-#         for(int i=0;i<nums.size();i++){
-#             for(int j=i+1;j<nums.size();j++){
-#                 if(nums[i]+nums[j]==target)
-#                     return {i,j};
-#             }
-#         }
-        
-#         return {};
-#     }
-    
-# };
-
-
-
